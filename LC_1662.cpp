dca84214f579b8b338971ebc6996607ab2bf3526
Nikhil Sharma <nikhilsharma.csgeek@gmail.com>
2022-01-03 18:08:00 +0530

    03-01-2021_LC1662

---
+++ LC_1662.cpp
@@ -0,0 +1,19 @@
+// class Solution {
+// public:
+//     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        
+//         string one="";
+//         string two="";
+        
+//         for(string x:word1)
+//             one=one+x;
+        
+//         for(string x:word2)
+//             two+=x;
+        
+//         if(one==two)
+//             return true;
+        
+//         return false;
+//     }
+// };
