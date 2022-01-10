c16cd8b6048e90708ad41ab47e2ff576b69fc43b
Nikhil Sharma <nikhilsharma.csgeek@gmail.com>
2022-01-10 12:04:13 +0530

    10-01-2022_LC1450

---
+++ LC_1450.cpp
@@ -0,0 +1,16 @@
+// class Solution {
+// public:
+//     int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        
+//         int ans=0;
+//         int n=startTime.size();
+        
+//         for(int i=0;i<n;i++){
+            
+//             if(startTime[i]<=queryTime&&queryTime<=endTime[i])
+//                 ans++;
+//         }
+        
+//         return ans;
+//     }
+// };
